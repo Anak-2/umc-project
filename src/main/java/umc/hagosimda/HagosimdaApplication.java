fac9c5e9006c63d60fdbb106773ebb735b959f34
donghyun1208 <ilovekdh1208@ajou.ac.kr>
2024-01-14 22:03:40 +0900

    Docs: init project

---
+++ src/main/java/umc/hagosimda/HagosimdaApplication.java
@@ -0,0 +1,13 @@
+package umc.hagosimda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HagosimdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HagosimdaApplication.class, args);
+	}
+
+}
